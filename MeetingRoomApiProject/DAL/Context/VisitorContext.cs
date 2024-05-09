263589a1476936c5f98ba1e52c4cd39470c81ec8
cekdarcpr <cekooo12@gmail.com>
2024-05-09 20:54:50 +0300

    Add project files.

---
+++ MeetingRoomApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,16 @@
+﻿using MeetingRoomApiProject.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomApiProject.DAL.Context
+{
+    public class VisitorContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-KNM9LLL\\SQLEXPRESS;initial catalog=ReservationDbApi;integrated security=true;");
+
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
